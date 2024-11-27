34358eb64f3fd5beeaaa4b7c0484474f279fc1dc
Shadrach Suresh <ssuresh4@umd.edu>
2024-11-27 17:56:31 -0500

    added secret & host

---
+++ flask_app/config.py
@@ -0,0 +1,3 @@
+# Stores all configuration values
+SECRET_KEY = b'\x82\x159\x92\xaa\xa0\x06\x11\x843\x94\x9c\xe7\xaf9\xa6'
+MONGODB_HOST = 'mongodb+srv://nico:388j@cluster0.f22qd.mongodb.net/388jP3?retryWrites=true&w=majority&appName=Cluster0'
